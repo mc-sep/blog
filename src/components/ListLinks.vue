d6fcca03340c402f2ef91029646f5b00107e114f
isYangs <hi@isyangs.cn>
2023-12-13 17:03:14 +0800

    fix: 修改友链页面站标链接

--- src/components/ListLinks.vue
+++ src/components/ListLinks.vue
@@ -50,7 +50,7 @@ function slug(name: string) {
           <p>name ：isYangs Blog (站点名称)</p>
           <p>desc ：一个前端Bug构造师的博客 (站点描述)</p>
           <p>link ：https://isyangs.cn (站点链接)</p>
-          <p>logo ：https://isyangs.cn/avatar.jpg (站点图标)</p>
+          <p>logo ：https://7.isyangs.cn/8/655c9835780a0-8.jpg (站点图标)</p>
         </blockquote>
       </p>
     </div>
